eae02e0d351bcb70dcc7b1e63c36c3c349a18024
Tybugs <marty.aicpa@gmail.com>
2022-06-13 20:59:21 +0800

    fix by jim

--- src/utils/commonData.js
+++ src/utils/commonData.js
@@ -1,7 +1,7 @@
 // const apiUrl = 'http://192.168.1.4:23000/api';
 // const apiUrl = '/api';
 // const apiUrl = '/api';
-const apiUrl = 'http://docampaign.online:23000/api';
+const apiUrl = 'https://docampaign.online/api';
 // const apiUrl = 'http://192.168.1.21:23000/api';
 
 const webopt = 'https://developer.globelabs.com.ph/dialog/oauth/kA8aH5keEgCMLTKkRbieqkCGeAroH5KL';
