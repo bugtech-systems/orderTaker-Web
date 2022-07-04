3dbedd229419dfcbae91ec2109122d43f36ed53a
Mark Steven Alba <salba@highlysucceed.com>
2022-07-05 02:18:51 +0800

    updates

--- src/@fake-db/modules/products.js
+++ src/@fake-db/modules/products.js
@@ -36,8 +36,8 @@ export const products = [
     folder: 'products',
   },
   {
-    id: 1457690401,
-    name: 'Chelsea Johns',
+    id: 4800888209566,
+    name: 'Cream Silk - Triple Keratin',
     dpUrl: 'https://via.placeholder.com/150',
     email_address: 'chelsea-johnss@example.com',
     phones: [{ phone: '(215)-659-7529', label: 'home' }],
@@ -49,8 +49,8 @@ export const products = [
     folder: 'products',
   },
   {
-    id: 1457690402,
-    name: 'Domnic Brown',
+    id: 6958898101689,
+    name: 'Pentel Pen - BLUE',
     dpUrl: 'https://via.placeholder.com/150',
     email_address: 'domnic-brown@example.com',
     phones: [{ phone: '(215)-748-7855', label: 'home' }],
--- src/services/http-api/crud.service.js
+++ src/services/http-api/crud.service.js
@@ -1,416 +1,388 @@
-import axios from "axios";
+import axios from 'axios';
 
-import constant from '../../utils/commonData'
+import constant from '../../utils/commonData';
 
-import httpHelpers  from './auth-header';
-
-
-
-
-
+import httpHelpers from './auth-header';
 
 //Roles
-const createRole = (data) => {
+const createRole = data => {
   return axios.post(constant.apiUrl + `/roles`, data);
 };
 
 const getRoles = () => {
-    return axios.get(constant.apiUrl + `/roles`);
+  return axios.get(constant.apiUrl + `/roles`);
 };
 
-const getRoleById = (id) => {
-    return axios.get(constant.apiUrl + `/role/${id}`);
+const getRoleById = id => {
+  return axios.get(constant.apiUrl + `/role/${id}`);
 };
-
 
 const updateRoleById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/role/${id}`, data);
+  return axios.patch(constant.apiUrl + `/role/${id}`, data);
 };
 
-const deleteRoleById = (id) => {
-    return axios.delete(constant.apiUrl + `/role/${id}`);
+const deleteRoleById = id => {
+  return axios.delete(constant.apiUrl + `/role/${id}`);
 };
-
 
 //Users
 
-const createUser = (data) => {
-    return axios.post(constant.apiUrl + "/user", data, { headers: httpHelpers.authHeader() } );
+const createUser = data => {
+  return axios.post(constant.apiUrl + '/user', data, {
+    headers: httpHelpers.authHeader(),
+  });
 };
 
 const getUsers = () => {
-    return axios.get(constant.apiUrl + `/users`, { headers: httpHelpers.authHeader() } );
+  return axios.get(constant.apiUrl + `/users`, {
+    headers: httpHelpers.authHeader(),
+  });
 };
 
-const getUserById = (id) => {
-    return axios.get(constant.apiUrl + `/user/${id}`, { headers: httpHelpers.authHeader() });
+const getUserById = id => {
+  return axios.get(constant.apiUrl + `/user/${id}`, {
+    headers: httpHelpers.authHeader(),
+  });
 };
-
 
 const updateUserById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/user/${id}`, data, { headers: httpHelpers.authHeader() } );
+  return axios.patch(constant.apiUrl + `/user/${id}`, data, {
+    headers: httpHelpers.authHeader(),
+  });
 };
 
-const deleteUserById = (id) => {
-    return axios.delete(constant.apiUrl + `/user/${id}`, { headers: httpHelpers.authHeader() } );
+const deleteUserById = id => {
+  return axios.delete(constant.apiUrl + `/user/${id}`, {
+    headers: httpHelpers.authHeader(),
+  });
 };
-
 
 //Customers
-const createCustomer = (data) => {
-    return axios.post(constant.apiUrl + "/customer", data);
+const createCustomer = data => {
+  return axios.post(constant.apiUrl + '/customer', data);
 };
 
 const getCustomers = () => {
-    return axios.get(constant.apiUrl + "/customers");
+  return axios.get(constant.apiUrl + '/customers');
 };
 
-const getCustomerById = (id) => {
-    return axios.get(constant.apiUrl + `/customer/${id}`);
+const getCustomerById = id => {
+  return axios.get(constant.apiUrl + `/customer/${id}`);
 };
-
 
 const updateCustomerById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/customer/${id}`, data);
+  return axios.patch(constant.apiUrl + `/customer/${id}`, data);
 };
 
-const deleteCustomerById = (id) => {
-    return axios.delete(constant.apiUrl + `/customer/${id}`);
+const deleteCustomerById = id => {
+  return axios.delete(constant.apiUrl + `/customer/${id}`);
 };
-
-
 
 //Purchases
-const createPurchases = (data) => {
-    return axios.post(constant.apiUrl + "/purchases", data);
+const createPurchases = data => {
+  return axios.post(constant.apiUrl + '/purchases', data);
 };
 
 const getPurchases = () => {
-    return axios.get(constant.apiUrl + "/purchases");
+  return axios.get(constant.apiUrl + '/purchases');
 };
 
-const getPurchaseById = (id) => {
-    return axios.get(constant.apiUrl + `/purchase/${id}`);
+const getPurchaseById = id => {
+  return axios.get(constant.apiUrl + `/purchase/${id}`);
 };
-
 
 const updatePurchaseById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/purchase/${id}`, data);
+  return axios.patch(constant.apiUrl + `/purchase/${id}`, data);
 };
 
-const deletePurchaseById = (id) => {
-    return axios.delete(constant.apiUrl + `/purchase/${id}`);
+const deletePurchaseById = id => {
+  return axios.delete(constant.apiUrl + `/purchase/${id}`);
 };
 
 //Products
-const createProduct = (data) => {
-    return axios.post(constant.apiUrl + "/product", data);
+const createProduct = data => {
+  return axios.post(constant.apiUrl + '/product', data);
 };
 
 const getProducts = () => {
-    return axios.get(constant.apiUrl + "/products");
+  return axios.get(constant.apiUrl + '/products');
 };
 
-const getProductById = (id) => {
-    return axios.get(constant.apiUrl + `/product/${id}`);
+const getProductById = id => {
+  return axios.get(constant.apiUrl + `/product/${id}`);
 };
-
 
 const updateProductById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/product/${id}`, data);
+  return axios.patch(constant.apiUrl + `/product/${id}`, data);
 };
 
-const deleteProductById = (id) => {
-    return axios.delete(constant.apiUrl + `/product/${id}`);
+const deleteProductById = id => {
+  return axios.delete(constant.apiUrl + `/product/${id}`);
 };
-
 
 //Payments
-const createPayment = (data) => {
-    return axios.post(constant.apiUrl + "/payment", data);
+const createPayment = data => {
+  return axios.post(constant.apiUrl + '/payment', data);
 };
 
 const getPayments = () => {
-    return axios.get(constant.apiUrl + "/payments");
+  return axios.get(constant.apiUrl + '/payments');
 };
 
-const getPaymentById = (id) => {
-    return axios.get(constant.apiUrl + `/payment/${id}`);
+const getPaymentById = id => {
+  return axios.get(constant.apiUrl + `/payment/${id}`);
 };
-
 
 const updatePaymentById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/payment/${id}`, data);
+  return axios.patch(constant.apiUrl + `/payment/${id}`, data);
 };
 
-const deletePaymentById = (id) => {
-    return axios.delete(constant.apiUrl + `/payment/${id}`);
+const deletePaymentById = id => {
+  return axios.delete(constant.apiUrl + `/payment/${id}`);
 };
 
-
- //Pricings
-
-const createPricing = (data) => {
-    return axios.post(constant.apiUrl + "/pricing", data);
+//Pricings
+const createPricing = data => {
+  return axios.post(constant.apiUrl + '/pricing', data);
 };
 
 const getPricings = () => {
-    return axios.get(constant.apiUrl + "/pricings");
+  return axios.get(constant.apiUrl + '/pricings');
 };
 
-const getPricingById = (id) => {
-    return axios.get(constant.apiUrl + `/pricing/${id}`);
+const getPricingById = id => {
+  return axios.get(constant.apiUrl + `/pricing/${id}`);
 };
-
 
 const updatePricingById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/pricing/${id}`, data);
+  return axios.patch(constant.apiUrl + `/pricing/${id}`, data);
 };
 
-const deletePricingById = (id) => {
-    return axios.delete(constant.apiUrl + `/pricing/${id}`);
+const deletePricingById = id => {
+  return axios.delete(constant.apiUrl + `/pricing/${id}`);
 };
-
 
 //Orders
 
-const createOrder = (data) => {
-    return axios.post(constant.apiUrl + "/order", data);
+const createOrder = data => {
+  return axios.post(constant.apiUrl + '/order', data);
 };
 
 const getOrders = () => {
-    return axios.get(constant.apiUrl + "/orders");
+  return axios.get(constant.apiUrl + '/orders');
 };
 
-const getOrderById = (id) => {
-    return axios.get(constant.apiUrl + `/order/${id}`);
+const getOrderById = id => {
+  return axios.get(constant.apiUrl + `/order/${id}`);
 };
-
 
 const updateOrderById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/order/${id}`, data);
+  return axios.patch(constant.apiUrl + `/order/${id}`, data);
 };
 
-const deleteOrderById = (id) => {
-    return axios.delete(constant.apiUrl + `/order/${id}`);
+const deleteOrderById = id => {
+  return axios.delete(constant.apiUrl + `/order/${id}`);
 };
-
-
 
 //Order_items
 
-const createOrder_item = (data) => {
-    return axios.post(constant.apiUrl + "/order_item", data);
+const createOrder_item = data => {
+  return axios.post(constant.apiUrl + '/order_item', data);
 };
 
 const getOrder_items = () => {
-    return axios.get(constant.apiUrl + "/order_items");
+  return axios.get(constant.apiUrl + '/order_items');
 };
 
-const getOrder_itemById = (id) => {
-    return axios.get(constant.apiUrl + `/order_item/${id}`);
+const getOrder_itemById = id => {
+  return axios.get(constant.apiUrl + `/order_item/${id}`);
 };
-
 
 const updateOrder_itemById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/order_item/${id}`, data);
+  return axios.patch(constant.apiUrl + `/order_item/${id}`, data);
 };
 
-const deleteOrder_itemById = (id) => {
-    return axios.delete(constant.apiUrl + `/order_item/${id}`);
+const deleteOrder_itemById = id => {
+  return axios.delete(constant.apiUrl + `/order_item/${id}`);
 };
-
-
 
 //Notifications
 
-const createNotification = (data) => {
-    return axios.post(constant.apiUrl + "/notification", data);
+const createNotification = data => {
+  return axios.post(constant.apiUrl + '/notification', data);
 };
 
 const getNotifications = () => {
-    return axios.get(constant.apiUrl + "/notifications");
+  return axios.get(constant.apiUrl + '/notifications');
 };
 
-const getNotificationById = (id) => {
-    return axios.get(constant.apiUrl + `/notification/${id}`);
+const getNotificationById = id => {
+  return axios.get(constant.apiUrl + `/notification/${id}`);
 };
-
 
 const updateNotificationById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/notification/${id}`, data);
+  return axios.patch(constant.apiUrl + `/notification/${id}`, data);
 };
 
-const deleteNotificationById = (id) => {
-    return axios.delete(constant.apiUrl + `/notification/${id}`);
+const deleteNotificationById = id => {
+  return axios.delete(constant.apiUrl + `/notification/${id}`);
 };
-
 
 //Inventories
 
-const createInventory = (data) => {
-    return axios.post(constant.apiUrl + "/inventory", data);
+const createInventory = data => {
+  return axios.post(constant.apiUrl + '/inventory', data);
 };
 
 const getInventories = () => {
-    return axios.get(constant.apiUrl + "/inventories");
+  return axios.get(constant.apiUrl + '/inventories');
 };
 
-const getInventoryById = (id) => {
-    return axios.get(constant.apiUrl + `/inventory/${id}`);
+const getInventoryById = id => {
+  return axios.get(constant.apiUrl + `/inventory/${id}`);
 };
-
 
 const updateInventoryById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/inventory/${id}`, data);
+  return axios.patch(constant.apiUrl + `/inventory/${id}`, data);
 };
 
-const deleteInventoryById = (id) => {
-    return axios.delete(constant.apiUrl + `/inventory/${id}`);
+const deleteInventoryById = id => {
+  return axios.delete(constant.apiUrl + `/inventory/${id}`);
 };
-
 
 //Categories
 
-const createCategory = (data) => {
-    return axios.post(constant.apiUrl + "/category", data);
+const createCategory = data => {
+  return axios.post(constant.apiUrl + '/category', data);
 };
 
 const getCategories = () => {
-    return axios.get(constant.apiUrl + "/categories");
+  return axios.get(constant.apiUrl + '/categories');
 };
 
-const getCategoryById = (id) => {
-    return axios.get(constant.apiUrl + `/category/${id}`);
+const getCategoryById = id => {
+  return axios.get(constant.apiUrl + `/category/${id}`);
 };
-
 
 const updateCategoryById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/category/${id}`, data);
+  return axios.patch(constant.apiUrl + `/category/${id}`, data);
 };
 
-const deleteCategoryById = (id) => {
-    return axios.delete(constant.apiUrl + `/category/${id}`);
+const deleteCategoryById = id => {
+  return axios.delete(constant.apiUrl + `/category/${id}`);
 };
-
-
 
 //Business
 
-const createBusiness = (data) => {
-    return axios.post(constant.apiUrl + "/business", data);
+const createBusiness = data => {
+  return axios.post(constant.apiUrl + '/business', data);
 };
 
 const getBusinesses = () => {
-    return axios.get(constant.apiUrl + "/businesses");
+  return axios.get(constant.apiUrl + '/businesses');
 };
 
-const getBusinessById = (id) => {
-    return axios.get(constant.apiUrl + `/business/${id}`);
+const getBusinessById = id => {
+  return axios.get(constant.apiUrl + `/business/${id}`);
 };
-
 
 const updateBusinessById = (id, data) => {
-    return axios.patch(constant.apiUrl + `/business/${id}`, data);
+  return axios.patch(constant.apiUrl + `/business/${id}`, data);
 };
 
-const deleteBusinessById = (id) => {
-    return axios.delete(constant.apiUrl + `/business/${id}`);
+const deleteBusinessById = id => {
+  return axios.delete(constant.apiUrl + `/business/${id}`);
 };
-
-
-
 
 export default {
-    //Roles
-    createRole,
-    getRoles,
-    getRoleById,
-    updateRoleById,
-    deleteRoleById,
+  //Roles
+  createRole,
+  getRoles,
+  getRoleById,
+  updateRoleById,
+  deleteRoleById,
 
-    //Users
-    createUser,
-    getUsers,
-    getUserById,
-    updateUserById,
-    deleteUserById,
+  //Users
+  createUser,
+  getUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
 
-    //Customers
-    createCustomer,
-    getCustomers,
-    getCustomerById,
-    updateCustomerById,
-    deleteCustomerById,
-    
-    //Purchases
-    createPurchases,
-    getPurchases,
-    getPurchaseById,
-    updatePurchaseById,
-    deletePurchaseById,
+  //Customers
+  createCustomer,
+  getCustomers,
+  getCustomerById,
+  updateCustomerById,
+  deleteCustomerById,
 
-    //Products
-    createProduct,
-    getProducts,
-    getProductById,
-    updateProductById,
-    deleteProductById,
+  //Purchases
+  createPurchases,
+  getPurchases,
+  getPurchaseById,
+  updatePurchaseById,
+  deletePurchaseById,
 
-    //Payments
-    createPayment,
-    getPayments,
-    getPaymentById,
-    updatePaymentById,
-    deletePaymentById,
+  //Products
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
 
-    //Pricings
-    createPricing,
-    getPricings,
-    getPricingById,
-    updatePricingById,
-    deletePricingById,
+  //Payments
+  createPayment,
+  getPayments,
+  getPaymentById,
+  updatePaymentById,
+  deletePaymentById,
 
-    //Orders
-    createOrder,
-    getOrders,
-    getOrderById,
-    updateOrderById,
-    deleteOrderById,
+  //Pricings
+  createPricing,
+  getPricings,
+  getPricingById,
+  updatePricingById,
+  deletePricingById,
 
-    //Order_items
-    createOrder_item,
-    getOrder_items,
-    getOrder_itemById,
-    updateOrder_itemById,
-    deleteOrder_itemById,
+  //Orders
+  createOrder,
+  getOrders,
+  getOrderById,
+  updateOrderById,
+  deleteOrderById,
 
-    //Notifications
-    createNotification,
-    getNotifications,
-    getNotificationById,
-    updateNotificationById,
-    deleteNotificationById,
+  //Order_items
+  createOrder_item,
+  getOrder_items,
+  getOrder_itemById,
+  updateOrder_itemById,
+  deleteOrder_itemById,
 
+  //Notifications
+  createNotification,
+  getNotifications,
+  getNotificationById,
+  updateNotificationById,
+  deleteNotificationById,
 
-    //Inventories
-    createInventory,
-    getInventories,
-    getInventoryById,
-    updateInventoryById,
-    deleteInventoryById,
+  //Inventories
+  createInventory,
+  getInventories,
+  getInventoryById,
+  updateInventoryById,
+  deleteInventoryById,
 
-    //Categories
-    createCategory,
-    getCategories,
-    getCategoryById,
-    updateCategoryById,
-    deleteCategoryById,
+  //Categories
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
 
-    //Business
-    createBusiness,
-    getBusinesses,
-    getBusinessById,
-    updateBusinessById,
-    deleteBusinessById,
+  //Business
+  createBusiness,
+  getBusinesses,
+  getBusinessById,
+  updateBusinessById,
+  deleteBusinessById,
 };
