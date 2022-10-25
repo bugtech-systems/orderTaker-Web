210408586f6fb3701d62234ed0d6a3c1dabfe3a8
leikezej <jezedevkiel21@gmail.com>
2022-10-26 04:02:36 +0800

    Products List Change To Pagination View

--- src/routes/Pages/Dashboard/Dashboard/OurStore/index.js
+++ src/routes/Pages/Dashboard/Dashboard/OurStore/index.js
@@ -2,26 +2,21 @@ import React, { useState } from 'react';
 import CmtCard from '../../../../../@coremat/CmtCard';
 import CmtCardHeader from '../../../../../@coremat/CmtCard/CmtCardHeader';
 import CmtCardContent from '../../../../../@coremat/CmtCard/CmtCardContent';
-import { useDispatch, useSelector } from 'react-redux';
+// import { GoogleMap, withGoogleMap } from 'react-google-maps';
 
 import Box from '@material-ui/core/Box';
+// import AppSelectBox from '../../../../../@jumbo/components/Common/formElements/AppSelectBox';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import CallIcon from '@material-ui/icons/Call';
-import EditIcon from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
-
+// import IconButton from '@material-ui/core/IconButton';
 import { intranet } from '../../../../../@fake-db';
 import { alpha, makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import { blue, pink } from '@material-ui/core/colors';
-
-import { SET_USER_DIALOG } from 'redux/actions/types';
-import { setCurrentUser } from 'redux/actions/Users';
-import commonData from 'utils/commonData';
-
-// import { SET_STORE_DIALOG } from 'redux/actions/types';
-// import { setCurrentStore } from 'redux/actions/Users';
-
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import InstagramIcon from '@material-ui/icons/Instagram';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const useStyles = makeStyles(theme => ({
   cardRoot: {
@@ -82,38 +77,42 @@ const useStyles = makeStyles(theme => ({
       fontSize: 20,
     },
   },
-  editIcon: {
-    marginLeft: 790,
-    marginTop: 10
-  },
 }));
 
-const OurStore = () => {
-  const { addresses, description, business } = intranet.ourOfficeData;
-  const { name, address, title, contact, currentAddress } = business ? business : {};
+// const socialMediaLinks = [
+//   { title: 'twitter', url: '', icon: <TwitterIcon /> },
+//   { title: 'facebook', url: '', icon: <FacebookIcon /> },
+//   { title: 'instagram', url: '', icon: <InstagramIcon /> },
+//   { title: 'linkedin', url: '', icon: <LinkedInIcon /> },
+// ];
+
+const OurStore = ({ business }) => {
+  const { addresses } = intranet.ourOfficeData;
+  const { name, address } = business ? business : {};
   const [currentAddress] = useState(addresses[0]);
-  const { authUser } = useSelector(({auth}) => auth);
-  // const { authStore } = useSelector(({auth}) => auth);
-
   const classes = useStyles();
-  const dispatch = useDispatch();
+  // const GoogleMapBox = withGoogleMap(() => (
+  // <GoogleMap defaultZoom={4} defaultCenter={{ lat: 20.75056525, lng: 73.730039 }} />
+  // ));
 
-  const handleEdit = () => {
-    dispatch(setCurrentUser(authUser));
-    dispatch({type: SET_USER_DIALOG, payload: true});
-  }
-
-  // const handleEdit = () => {
-  //   dispatch(setCurrentStore(authStore));
-  //   dispatch({type: SET_STORE_DIALOG, payload: true});
-  // }
+  // const handleAddressChange = event => {
+  //   setAddress(addresses.find(item => item.label === event.target.value));
+  // };
 
   return (
     <CmtCard className={classes.cardRoot}>
       <CmtCardHeader title={name} subTitle={address} />
-        <IconButton onClick={(e) => handleEdit()} style={{position: 'absolute', right: '5px', zIndex: 5}} color="primary"><EditIcon  /></IconButton>
 
       <CmtCardContent>
+        {/* <Box className={classes.selectBoxRoot}>
+          <AppSelectBox
+            data={addresses}
+            valueKey="label"
+            labelKey="label"
+            value={currentAddress.label}
+            onChange={handleAddressChange}
+          />
+        </Box> */}
         <Box mb={5}>
           <Typography component="div" variant="h4" className={classes.addressTitle}>
             Contact Details
@@ -134,10 +133,24 @@ const OurStore = () => {
             <Box ml={4}>{currentAddress.emailAddress}</Box>
           </Box>
         </Box>
+        {/* 
+        <Box className={classes.socialLink}>
+          {socialMediaLinks.map((item, index) => (
+            <Box key={index} className={classes.socialLinkCol}>
+              <IconButton className={`btn ${item.title}`}>{item.icon}</IconButton>
+            </Box>
+          ))}
+        </Box> */}
       </CmtCardContent>
+      {/* <Box p={2} height={1}>
+        <GoogleMapBox
+          loadingElement={<Box height={1} />}
+          containerElement={<Box height={1} />}
+          mapElement={<Box height={1} />}
+        />
+      </Box> */}
     </CmtCard>
   );
-
 };
 
 export default OurStore;
--- src/routes/Pages/Inventory/ProductList/ProductsList/ListTableView/index.js
+++ src/routes/Pages/Inventory/ProductList/ProductsList/ListTableView/index.js
@@ -14,8 +14,6 @@ import NoRecordFound from './NoRecordFound';
 //Jumbo
 import { getComparator, stableSort } from '../../../../../../@jumbo/utils/tableHelper';
 
-
-
 const ListTableView = ({
   checkedProducts,
   handleCellCheckBox,
@@ -55,42 +53,7 @@ const classes = useStyles();
       )}
       <Box className="Cmt-table-responsive">
         <Table>
-          {checkedProducts.length === 0 && (
-                     <ProductTableHead
-                     classes={classes}
-                     numSelected={checkedProducts.length}
-                     order={order}
-                     orderBy={orderBy}
-                     onSelectAllClick={handleHeaderCheckBox}
-                     onRequestSort={handleRequestSort}
-                     rowCount={productsList.length}
-
-                   />
-          )}
-          <TableBody>
-          {!!productsList.length ? (
-                stableSort(productsList, getComparator(order, orderBy))
-                  .map((row, index) => (
-                    <ProductCell
-                    key={index}
-                    product={row}
-                    checkedProducts={checkedProducts}
-                    handleCellCheckBox={handleCellCheckBox}
-                    onShowProductDetail={onShowProductDetail}
-                    onClickEditProduct={onClickEditProduct}
-                    onClickAddStocks={onClickAddStocks}
-                    onDelete={onDelete}
-                  />
-                  ))
-              ) : (
-                <TableRow style={{ height: 53 * 6 }}>
-                <TableCell colSpan={7} rowSpan={10}>
-                    <NoRecordFound>There are no records found with your filter.</NoRecordFound>
-                </TableCell>
-              </TableRow>
-              ) 
-              }
-          </TableBody>
+            <p>hehe</p>
         </Table>
       </Box>
     </React.Fragment>
