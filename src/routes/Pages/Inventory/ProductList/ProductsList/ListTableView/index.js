25a37dc7485b1639d45a03d281ee7c225f482ff8
jeze kiel <jezedevkiel21@gmail.com>
2022-10-26 11:11:44 +0800

    testing

--- src/routes/Pages/Inventory/ProductList/ProductsList/ListTableView/index.js
+++ src/routes/Pages/Inventory/ProductList/ProductsList/ListTableView/index.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import { makeStyles } from "@material-ui/core/styles";
 
 import ProductTableHead from './ProductTableHead';
 import Table from '@material-ui/core/Table';
@@ -10,38 +9,11 @@ import Paper from "@material-ui/core/Paper";
 import ProductCell from './ProductCell';
 import CheckedListHeader from './CheckedListHeader';
 import PropTypes from 'prop-types';
-import Box from '@material-ui/core/Box';
 import useStyles from './index.style';
 import NoRecordFound from './NoRecordFound';
 
-
 //Jumbo
 import { getComparator, stableSort } from '../../../../../../@jumbo/utils/tableHelper';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, ),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread2", 356, 16.0, 49, 3.9),
-  createData("Gingerbread3", 356, 16.0, 49, 3.9),
-  createData("Gingerbread4", 356, 16.0, 49, 3.9),
-  createData("Gingerbread5", 356, 16.0, 49, 3.9),
-  createData("Gingerbread6", 356, 16.0, 49, 3.9),
-  createData("Gingerbread7", 356, 16.0, 49, 3.9),
-  createData("Gingerbread8", 356, 16.0, 49, 3.9),
-  createData("Gingerbread9", 356, 16.0, 49, 3.9),
-  createData("Gingerbread10", 356, 16.0, 49, 3.9),
-  createData("Gingerbread11", 356, 16.0, 49, 3.9),
-  createData("Gingerbread12", 356, 16.0, 49, 3.9),
-  createData("Gingerbread13", 356, 16.0, 49, 3.9)
-];
-
 
 const ListTableView = ({
   checkedProducts,
@@ -54,18 +26,26 @@ const ListTableView = ({
   onDelete
 }) => {
 const classes = useStyles();
+  const { users } = useSelector((state) => state.usersReducer);
+  const { userDialog } = useSelector((state) => state.uiReducer);
+  const [selected, setSelected] = React.useState([]);
+
   const [orderBy, setOrderBy] = React.useState('name');
   const [order, setOrder] = React.useState('asc');
   const { productsList } = useSelector(({ productApp }) => productApp);
   const [page, setPage] = React.useState(0);
   const [products, setProducts] = useState([])
+  // const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [selectedUser, setSelectedUser] = useState({ name: '' });
+  // const [usersFetched, setUsersFetched] = useState(false);
+  // const [currentUser, setCurrent] = useState({});
   
   const fetchData = async () => {
   const response = await fetch("http://localhost:3001/api/products")
   const data = await response.json()
-  setProducts(data)
+   setProducts(data)
+  //  setProducts(newProducts)
   }
-
   useEffect(() => {
     fetchData()
   }, [])
@@ -75,19 +55,56 @@ const classes = useStyles();
     setPage(newPage);
   };
   
-    const handleChangeRowsPerPage = event => {
+  const handleChangeRowsPerPage = event => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
   
-    const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    // const emptyRows =
+    // rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrderBy(property);
     setOrder(isAsc ? 'desc' : 'asc');
   };
+  
+    const handleSelectAllClick = event => {
+    if (event.target.checked) {
+      const newSelected = users.map(n => n.id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleRowClick = (event, id) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+
+    setSelected(newSelected);
+  };
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleRowsPerPageChange = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  
+  const isSelected = id => selected.indexOf(id) !== -1;
+  
 
   return (
     <React.Fragment>
@@ -116,10 +133,10 @@ const classes = useStyles();
           <TableBody>
        {products
             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((row, index) => (
+            .map((data, index) => (
                     <ProductCell
                     key={index}
-                    product={row}
+                    product={data}
                     checkedProducts={checkedProducts}
                     handleCellCheckBox={handleCellCheckBox}
                     onShowProductDetail={onShowProductDetail}
@@ -128,12 +145,17 @@ const classes = useStyles();
                     onDelete={onDelete}
                   />
             ))}
+                      {/* {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )} */}
           </TableBody>
       </Table>
       <TablePagination
         rowsPerPageOptions={[1, 5, 10, 25, 50]}
         component="div"
-        count={rows.length}
+        count={products.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={handleChangePage}
