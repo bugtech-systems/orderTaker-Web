edb76eb95edd2fed513fdbcfd05e3ace30b2ddc0
jGeli <jaybee.aicpa@gmail.com>
2023-02-18 03:46:33 +0800

    Fix build

---
+++ build/precache-manifest.a5ab182cadca6e78c0f73cba2f026592.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e6b7c2fa74b38aac865ac03137f616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3492cd56eaf45a72785f",
+    "url": "/static/css/2.dfb5b76e.chunk.css"
+  },
+  {
+    "revision": "0a4915b2f35056c95366",
+    "url": "/static/css/main.5d7f95b7.chunk.css"
+  },
+  {
+    "revision": "3492cd56eaf45a72785f",
+    "url": "/static/js/2.f6efe4cd.chunk.js"
+  },
+  {
+    "revision": "576eac0022eafc88c8b4c7e78240d555",
+    "url": "/static/js/2.f6efe4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac83a175442974c8f17",
+    "url": "/static/js/3.992d47ce.chunk.js"
+  },
+  {
+    "revision": "20761d442963b011a2af",
+    "url": "/static/js/4.efaccd30.chunk.js"
+  },
+  {
+    "revision": "2741121cc2ac36c79e61",
+    "url": "/static/js/5.690ffc8d.chunk.js"
+  },
+  {
+    "revision": "7724c66abf8f25ab149f",
+    "url": "/static/js/6.1a95858e.chunk.js"
+  },
+  {
+    "revision": "a96c98655c07eb4b627c",
+    "url": "/static/js/7.2aeed892.chunk.js"
+  },
+  {
+    "revision": "0a4915b2f35056c95366",
+    "url": "/static/js/main.a5862c0b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a5862c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fecb7e731639ccfc27",
+    "url": "/static/js/runtime-main.2e5ec2e5.js"
+  }
+]);
